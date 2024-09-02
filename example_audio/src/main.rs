852e6f18c7af079a8b643d073e5dc225388ac555
DoctorWhoof <home.leo.santos@gmail.com>
2024-09-02 16:34:26 -0700

    Basic audio support

---
+++ example_audio/src/main.rs
@@ -0,0 +1,51 @@
+use std::{f64::consts::TAU, i16};
+use mini_sdl::*;
+
+fn main() -> Result<(), String> {
+    let mut app = mini_sdl::App::new(
+        "audio-test",
+        320,
+        240,
+        Timing::VsyncLimitFPS(60.0),
+        Scaling::PreserveAspect,
+    )?;
+
+    let rate = app.mix_rate() as f64;
+    let frequency = 440.0;  // 440Hz, frequency of note A4
+    let period = 1.0 / frequency;
+    let time_per_sample = 1.0 / rate; // Time duration of one sample
+
+    let mut last_time = app.time();
+    let mut accumulated_phase = 0.0;
+    let mut samples = Vec::new();
+
+    app.start_audio();
+
+    while !app.quit_requested {
+        app.start_frame()?;
+        // Process audio
+        let current_time = app.time();
+        let time_delta = current_time - last_time;
+        last_time = current_time;
+        let samples_per_frame = (time_delta * rate).round() as usize;
+        // println!("Generating {} stereo samples per frame", samples_per_frame);
+        // Sine wave generation
+        for _ in 0..samples_per_frame {
+            let phase = (accumulated_phase % period) / period;
+            let sine_value = (TAU * phase).sin();
+            // Convert to i16 and push
+            let value =  (sine_value * 0.5 * i16::MAX as f64) as i16;
+            samples.push(StereoFrame {
+                left: value,
+                right: value,
+            });
+            accumulated_phase += time_per_sample;
+        }
+        // Copy new samples to app's audio buffer and reset samples container
+        app.push_audio_samples(samples.as_slice())?;
+        samples.clear();
+        // Always call "start" and "finish" frame!
+        app.finish_frame()?;
+    }
+    Ok(())
+}
---
+++ src/audio.rs
@@ -0,0 +1,58 @@
+use sdl2::audio::AudioCallback;
+use std::collections::VecDeque;
+
+
+/// A single audio sample, with left and right channels.
+#[derive(Debug, Default, Clone, Copy)]
+pub struct StereoFrame {
+    pub left: i16,
+    pub right: i16,
+}
+
+/// Used internally to push audio samples to the audio device. Although you can access it directly,
+/// it requires locking the audio device so it's easier to use 'App::push_audio_samples' instead.
+pub struct AudioInput {
+    buffer:  VecDeque<StereoFrame>,
+    last_frame: StereoFrame,
+    frame_count: usize,
+}
+
+impl AudioInput {
+    pub fn new() -> Self {
+        Self {
+            buffer: VecDeque::default(),
+            last_frame: StereoFrame::default(),
+            frame_count: 0,
+        }
+    }
+
+    /// For debugging purposes, returns lengh of internal buffer
+    pub fn buffer_len(&self) -> usize {
+        self.buffer.len()
+    }
+
+    /// Push a slice of StereoFrames. Ideally you should call this only once per frame,
+    /// with all the samples that you need for that frame.
+    #[inline(always)]
+    pub fn push_samples(&mut self, frames: &[StereoFrame]) {
+        for sample in frames.iter(){
+            self.buffer.push_back(*sample);
+        }
+    }
+}
+
+impl<'a> AudioCallback for AudioInput {
+    type Channel = i16;
+
+    fn callback(&mut self, out: &mut [i16]) {
+        for x in out.iter_mut() {
+            if self.frame_count % 2 == 0 {
+                self.last_frame = self.buffer.pop_front().unwrap_or_default();
+                *x = self.last_frame.left;
+            } else {
+                *x = self.last_frame.right;
+            }
+            self.frame_count += 1;
+        }
+    }
+}
